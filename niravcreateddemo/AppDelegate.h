a0dce23427efbed3ec3c38464c1e677ab346854a
Nirav <niravghori@gmail.com>
2014-09-29 12:12:32 +0530

    Initial Commit

---
+++ niravcreateddemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  niravcreateddemo
+//
+//  Created by Nirav on 29/09/14.
+//  Copyright (c) 2014 ample-arch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
